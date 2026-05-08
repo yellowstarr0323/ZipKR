bae2dc7ea2c6ef4fffa8b597031324e6ad05484b
yellowstarr0323 <ihansaem60@gmail.com>
2026-05-08 10:02:49 +0900

    project setting

---
+++ src/main/kotlin/com/example/zipkr/ZipKrApplication.kt
@@ -0,0 +1,11 @@
+package com.example.zipkr
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ZipKrApplication
+
+fun main(args: Array<String>) {
+    runApplication<ZipKrApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/zipkr/ZipKrApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.zipkr
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ZipKrApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
